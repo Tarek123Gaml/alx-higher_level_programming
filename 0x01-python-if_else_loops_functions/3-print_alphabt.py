991f13c95120b6833099208dc1f2deb53cec300a
tarek <taga61188@gmail.com>
2023-10-04 09:45:22 -0700

    loop

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for letter in range(ord('a'), ord('z') + 1):
     if chr(letter) not in 'eq':
-        print(chr(letter), end='')
+        print("{:s}".format(chr(letter)), end='')
