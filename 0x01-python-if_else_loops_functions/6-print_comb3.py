7fc4b453e2c53097180ce6f6e2ea4f7695e870dc
tarek <taga61188@gmail.com>
2023-10-04 11:09:06 -0700

    loop

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-for k in range(9):
+for k in range(8):
     for j in range(k + 1, 10):
-        if j != 9 & k !=  8:
             print(f"{k}{j}", end=", ")
-        else:
-            print(f"{k}{j}")
+print(f"{k + 1}{j}")
