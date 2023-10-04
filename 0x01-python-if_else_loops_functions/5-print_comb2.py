243512808c00d32199b23aba29a3d39eb10e2100
tarek <taga61188@gmail.com>
2023-10-04 10:09:37 -0700

    loop

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 for i in range(100):
-    print(f"{i // 10}{i % 10}", end="")
     if i != 99:
-        print(", ", end="")
+        print(f"{i // 10}{i % 10}, ", end="")
+    else:
+         print("99")
 
