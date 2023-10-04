b5c0369683842c3c75f9cdf1fe45247647ed81e7
tarek <taga61188@gmail.com>
2023-10-04 11:11:24 -0700

    loop

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for k in range(8):
     for j in range(k + 1, 10):
-            print(f"{k}{j}", end=", ")
-print(f"{k + 1}{j}")
+        print("{:d}{:d}".format(k, j), end=", ")
+print("{:d}{:d}".format(k + 1, j))
