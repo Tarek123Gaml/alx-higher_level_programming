17ddfbd20e542a8fe66bafcf1508c64c3e6386bf
tarek <taga61188@gmail.com>
2023-10-05 05:37:01 -0700

    import

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-import  sys
-import hidden_4
+import sys
+import hidden_4 as hidden
 
 if __name__ != "__main__":
     exit()
 
-for i in dir(hidden_4):
-    if i[:2] != "__":
-        print(i)
+for name in dir(hidden):
+    if name[0:2] != "__":
+        print(name)
