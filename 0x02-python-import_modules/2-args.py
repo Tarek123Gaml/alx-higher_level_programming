c440b6eff3753565593ace1a90fecefe564727ba
tarek <taga61188@gmail.com>
2023-10-05 05:08:53 -0700

    import

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,15 @@ if __name__ == "__main__":
     pass
 
 argc = len(sys.argv) - 1
-print("{:d} arguments:".format(argc))
+
+if arg == 0:
+    print("{:d} argument.".format(argc))
+
+elif argc == 1:
+    print("{:d} argument:".format(argc))
+
+else:
+    print("{:d} arguments:".format(argc))
 
 for i in range(argc):
     print("{:d}: {:s}".format(i + 1, sys.argv[i + 1]))
