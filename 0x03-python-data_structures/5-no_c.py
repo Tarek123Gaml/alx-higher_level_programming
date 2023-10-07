8c40cc949abb2359a3c7bb3ef9448f1dd207b82c
tarek <taga61188@gmail.com>
2023-10-06 22:44:03 -0700

    data structur

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,7 @@
 def no_c(my_string):
     l = 0
     for i in my_string:
-        if i is in 'cC':
+        if i is 'C' or i is 'c':
             my_string[l] = ''
         l += 1
     return my_string
