b84893c7364b4449ac0a69abcc6c74097decd0c8
tarek <taga61188@gmail.com>
2023-10-11 05:50:24 -0700

    dic & set

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,6 @@
 #!/sur/bin/python3
 def square_matrix_simple(matrix=[]):
     new_matrix = []
-
     for i in matrix:
         sub_matrix = map(lambda n: n**2, i)
         new_matrix.append(list(sub_matrix))
