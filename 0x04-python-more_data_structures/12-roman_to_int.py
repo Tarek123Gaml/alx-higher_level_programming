40874a2b20bab093aaa7f2289aac022239c46c84
tarek <taga61188@gmail.com>
2023-10-11 11:45:26 -0700

    dic

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     result = 0
     ronum = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    if type(roman_string) is not str or roman_string is None:
+    if (not isinstance(roman_string, str) or roman_string is None):
         return 0
     else:
         for i in roman_string:
