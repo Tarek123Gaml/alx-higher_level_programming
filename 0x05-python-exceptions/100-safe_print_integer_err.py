82370a0f3b1314bb0bcf362daee568759a1ccc5a
tarek <taga61188@gmail.com>
2023-10-25 00:31:39 -0700

    erorr and exception

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 import sys
 
+
 def safe_print_integer_err(value):
     re = True
 
     try:
         print("{:d}".format(value))
     except Exception as r:
-        print("Exception:", r, file = sys.stderr)
+        print("Exception:", r, file=sys.stderr)
         re = False
     return re
