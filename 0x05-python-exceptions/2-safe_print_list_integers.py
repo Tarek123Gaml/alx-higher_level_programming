d1869d3625bead7064f98a1e82f495b1637e2cfb
tarek <taga61188@gmail.com>
2023-10-24 22:49:03 -0700

    erorr and exception

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+   i, n = 0, 0
+   while i < x:
+       try:
+           print("{:d}".format(my_list[i]), end = '')
+           c += 1
+        except:
+            pass
+        i += 1
+    return c
