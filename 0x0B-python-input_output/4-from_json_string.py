220547a20b355d8ba8753cdd46d4c570d56452e1
tarek <taga61188@gmail.com>
2023-11-07 01:42:57 -0800

    class

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -5,7 +5,8 @@ import json
 
 def from_json_string(my_str):
     """
-    from_json_string - returns an object (Python data structure) represented by a JSON string
+    from_json_string - returns an object (Python data structure)
+                        represented by a JSON string
 
     Args:
         my_str: string we convert
