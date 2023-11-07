449ef648885e4c4eb424d4d0816e326283691195
tarek <taga61188@gmail.com>
2023-11-07 01:59:51 -0800

    class

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""load_from_json_file module"""
+import json
+
+
+def load_from_json_file(filename):
+    """
+    load_from_json_file - creates an Object from a “JSON file”
+
+    Args:
+        filename: name of the file
+    """
+    with open(filename, 'r') as f:
+        return (json.loads(f))
