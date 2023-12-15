5e3465c860a9b9bda777a72b1911b7b6094ca18c
tarek <taga61188@gmail.com>
2023-12-14 21:49:28 -0800

    databases

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
--- iiiii  that creates a table second_table:id INT, name VARCHAR(256), score INT 
+-- Creates and fills a table second_table with attributes id, name and score.
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
-INSERT INTO second_table VALUE (1, “John”, 10);
-INSERT INTO second_table VALUE (2, “Alex”, 3);
-INSERT INTO second_table VALUE (3, “Bob”, 14);
-INSERT INTO second_table VALUE (4, “George”, 8);
+INSERT INTO second_table VALUEs (1, "John", 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "George", 8);
