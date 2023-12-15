29ca2c62fa528a3d5f70d0fb5609077dcd57212a
tarek <taga61188@gmail.com>
2023-12-14 21:40:05 -0800

    databases

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,7 @@
+--iiiii  that creates a table second_table:id INT, name VARCHAR(256), score INT 
+-- reate these records:(1, “John”, 10)(2, “Alex”, 3)(3, “Bob”, 14)(4, “George”, 8)
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table VALUE (1, “John”, 10);
+INSERT INTO second_table VALUE (2, “Alex”, 3);
+INSERT INTO second_table VALUE (3, “Bob”, 14);
+INSERT INTO second_table VALUE (4, “George”, 8);
