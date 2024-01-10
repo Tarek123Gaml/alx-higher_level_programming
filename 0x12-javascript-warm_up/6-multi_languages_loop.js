43630ccecffae22cdf2dd4f901aadc357aedf40b
tarek <taga61188@gmail.com>
2024-01-09 21:20:53 -0800

    JS Warm up

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// prints 3 lines by using an array of string and a loop
+
+const langs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (a of langs) {
+  console.log(a);
+}
