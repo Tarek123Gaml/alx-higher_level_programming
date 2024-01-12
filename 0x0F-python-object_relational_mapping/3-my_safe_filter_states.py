6952f506affff3a47f025d0fa2c30d163ba6cd0f
tarek <taga61188@gmail.com>
2024-01-12 08:12:30 -0800

    Py ORM

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""lists dbs"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("""SELECT * FROM states WHERE name = '{}'
+            ORDER BY states.id ASC""".format(argv[4]))
+    rows = cur.fetchall()
+    for i in rows:
+        print(i)
