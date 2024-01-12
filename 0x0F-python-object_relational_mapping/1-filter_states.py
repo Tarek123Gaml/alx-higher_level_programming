75497a1ebba9db454d1190073c9464e2854f82bc
tarek <taga61188@gmail.com>
2024-01-12 06:52:16 -0800

    Py ORM

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""lists databases"""
+"""Lists states"""
 
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost",port=3306,user=argv[1],
-            passwd=argv[2],db=argv[3],charset="utf8")
-
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
     cur = conn.cursor()
-
     cur.execute("SELECT * FROM states ORDER BY states.id ASC")
-    dd = cur.fetchall()
-    for i in dd:
-        if i[1].startwith("N"):
-            print(i)
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        if row[1].startswith("N"):
+            print(row)
+    cur.close()
+    conn.close()
