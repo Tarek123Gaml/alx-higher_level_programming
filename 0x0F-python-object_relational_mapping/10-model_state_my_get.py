b9499854c0e2e7b0095eea3c7d4941a0629af16f
tarek <taga61188@gmail.com>
2024-01-13 01:32:39 -0800

    Py ORM

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     states = session.query(State).\
-            filter(State.name == argv[4]).order_by(State.id).all()
+        filter(State.name == argv[4]).order_by(State.id).all()
     if states:
         print("{}".format(states[0].id))
     else:
